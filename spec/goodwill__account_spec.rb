e5f6c44ceeb6dcc050b1484d6942792c4374c1b0
Brandon Burnett <gentoolicious@gmail.com>
2015-10-10 22:35:59 -0700

    Adding some fixtures and specs.

---
+++ spec/goodwill__account_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'pry'
+
+IN_PROG = 'https://www.shopgoodwill.com/buyers/myShop/AuctionsInProgress.asp'
+
+include Goodwill::URLPaths
+
+describe Goodwill::Account do
+
+  before do
+    mech = Mechanize.new
+    @account = Goodwill::Account.new('brandonburnett', 'butthat1')
+    in_prog_cont = mech.get('file:///' + File.expand_path('./spec/fixtures/in_progress.html'))
+    allow(@account.mechanize).to receive(:get).with(IN_PROG).and_return(in_prog_cont)
+    [ '24710437', '24763462', '24765885', '24766011'].each do |itemid|
+      content = mech.get('file:///' + File.expand_path("./spec/fixtures/#{itemid}.html"))
+      allow(@account.mechanize).to receive(:get).with(ITEM_SEARCH_URL + itemid).and_return(content)
+    end
+  end
+
+  describe '#bid' do
+    it "should be able to bid on auctions"
+  end
+
+  describe '#in_progress' do
+    it "should be able to get a list of auctions in progress" do
+      auctions = YAML.load(File.open('./spec/fixtures/auctions.yaml'))
+      expect(@account.in_progress).to eq(auctions)
+    end
+  end
+
+  describe '#search' do
+    it "should be able to search for auctions"
+  end
+
+end
