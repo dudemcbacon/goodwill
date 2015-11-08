21c0c0d3fbab42eb5cc63552047e36e3dcf846e7
Brandon Burnett <gentoolicious@gmail.com>
2015-11-07 18:01:11 -0800

    Fixing some CSS paths.

--- lib/goodwill/csspaths.rb
+++ lib/goodwill/csspaths.rb
@@ -4,7 +4,7 @@ module Goodwill
     # http://www.shopgoodwill.com/viewItem.asp?itemID=<itemID>
     BIDS_PATH = '#more_images'
     CURRENT_PRICE_PATH = 'body > span > div.itemdetail > div:nth-child(2) > div:nth-child(3) > div > table > tr:nth-child(2) > th'
-    END_TIME_PATH = 'body > span > div.itemdetail > div:nth-child(2) > div:nth-child(2) > div > table > tbody > tr:nth-child(6) > td'
+    END_TIME_PATH = 'body > span > div.itemdetail > div:nth-child(2) > div:nth-child(2) > div > table > tr:nth-child(5) > td'
     ITEMID_PATH = 'body > span > div.itemdetail > div:nth-child(2) > div:nth-child(2) > div > table > tbody > tr:nth-child(3) > td'
     ITEM_TITLE_PATH = '#title > span'
     SELLER_PATH = 'body > span > div.itemdetail > div:nth-child(2) > div:nth-child(2) > div > table > tr:nth-child(7) > td > b'
